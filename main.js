ff1e1de1263c489e16c7eb7ef149be3f422127eb
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-09 00:33:23 +0100

    main folder

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ function folderGenerated(){
     console.log("Folder was successfully created.");
 }
  */
+
+
+/* recursive true needed to create multiple folder */
+
 fs.mkdir(`${process.cwd()}/assets/css`,{recursive : true},(err) =>{
     if(err) throw err;
     console.log("YaaYYYY");
@@ -26,4 +30,18 @@ fs.mkdir(`${process.cwd()}/assets/fonts`,{recursive : true},(err) =>{
     if(err) throw err;
     console.log("YaaYYYY");
 }  )
+
+
+/* datei erstellen  */
+/* fs.appendFile('index.html','<link rel="stylesheet" href="./assets/css/style.css"<script src="./assets/js/main.js"></script>>' ,fileLog)
+
+function fileLog (){
+    console.log("yeAAAyy index file created");
+} */
+
+fs.appendFile('index.html','hi' ,fileLog)
+
+function fileLog (){
+    console.log("yeAAAyy index file created");
+}
 
