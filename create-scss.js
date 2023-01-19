340cd1f1fd05c2c584d041a3ca4b45904adf0197
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-19 19:48:45 +0100

    bug_fixed

--- create-scss.js
+++ create-scss.js
@@ -36,12 +36,12 @@ let folderCreator = () =>{
 /* creates files */
 let fileCreator = () =>{
     fs.writeFileSync('index.html','',     () => console.log("index.html has been created 🥳"));
-    fs.writeFileSync('assets/js/main.js','', () => console.log("main.js has been created 🥳"));
-    fs.writeFileSync('assets/css/style.css','', () => console.log("style.css has been created 🥳"));
-    fs.writeFileSync('assets/scss/style.scss','', () => console.log("style.scss has been created 🥳"));
-    fs.writeFileSync('assets/scss/_mixins.scss','', () => console.log("_mixins.scss has been created 🥳"));
-    fs.writeFileSync('assets/scss/_variables.scss','', () => console.log("_variables.scss has been created 🥳"));
-    fs.writeFileSync('assets/scss/_media-queries.scss','', () => console.log("_media-queries.scss has been created 🥳"));
+    fs.writeFileSync('assets/js/main.js','',() => console.log("main.js has been created 🥳"));
+    fs.writeFileSync('assets/css/style.css','',() => console.log("style.css has been created 🥳"));
+    fs.writeFileSync('assets/scss/style.scss','',() => console.log("style.scss has been created 🥳"));
+    fs.writeFileSync('assets/scss/_mixins.scss','',() => console.log("_mixins.scss has been created 🥳"));
+    fs.writeFileSync('assets/scss/_variables.scss','',() => console.log("_variables.scss has been created 🥳"));
+    fs.writeFileSync('assets/scss/_media-queries.scss','',() => console.log("_media-queries.scss has been created 🥳"));
 }
 
 for(let i = 0;i<=1;i++){
