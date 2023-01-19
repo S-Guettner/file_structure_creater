fdca3fa3f39c37a2d1048d448bb6e05e0e737964
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-19 19:43:36 +0100

    bug_fixed

--- create-scss.js
+++ create-scss.js
@@ -5,34 +5,37 @@ const path = require('path');
 
 /* recursive true needed to create  multiple folder */
 /* creates folder */
-fs.mkdir(`${process.cwd()}/assets`,{recursive : true},(err) =>{
-    if(err) throw console.log("An error has occured pls try again");
-    console.log("assets folder has been successfully created 🥳");
-}  )
-fs.mkdir(`${process.cwd()}/assets/css`,{recursive : true},(err) =>{
-    if(err) throw console.log("An error has occured pls try again");
-    console.log("assets / css folder has been successfully created 🥳");
-}  )
-fs.mkdir(`${process.cwd()}/assets/scss`,{recursive : true},(err) =>{
-    if(err) throw console.log("An error has occured pls try again");
-    console.log("assets / scss folder has been successfully created 🥳");
-}  )
-fs.mkdir(`${process.cwd()}/assets/js`,{recursive : true},(err) =>{
-    if(err) throw console.log("An error has occured pls try again");
-    console.log("assets / js folder has been successfully created 🥳");
-}  )
-fs.mkdir(`${process.cwd()}/assets/img`,{recursive : true},(err) =>{
-    if(err) throw console.log("An error has occured pls try again");
-    console.log("assets / img folder has been successfully created 🥳");
-}  )
-fs.mkdir(`${process.cwd()}/assets/fonts`,{recursive : true},(err) =>{
-    if(err) throw console.log("An error has occured pls try again");
-    console.log("assets / fonts folder has been successfully created 🥳");
-}  )
+let folderCreator = () =>{
+    fs.mkdir(`${process.cwd()}/assets`,{recursive : true},(err) =>{
+        if(err) throw console.log("An error has occured pls try again");
+        console.log("assets folder has been successfully created 🥳");
+    }  )
+    fs.mkdir(`${process.cwd()}/assets/css`,{recursive : true},(err) =>{
+        if(err) throw console.log("An error has occured pls try again");
+        console.log("assets / css folder has been successfully created 🥳");
+    }  )
+    fs.mkdir(`${process.cwd()}/assets/scss`,{recursive : true},(err) =>{
+        if(err) throw console.log("An error has occured pls try again");
+        console.log("assets / scss folder has been successfully created 🥳");
+    }  )
+    fs.mkdir(`${process.cwd()}/assets/js`,{recursive : true},(err) =>{
+        if(err) throw console.log("An error has occured pls try again");
+        console.log("assets / js folder has been successfully created 🥳");
+    }  )
+    fs.mkdir(`${process.cwd()}/assets/img`,{recursive : true},(err) =>{
+        if(err) throw console.log("An error has occured pls try again");
+        console.log("assets / img folder has been successfully created 🥳");
+    }  )
+    fs.mkdir(`${process.cwd()}/assets/fonts`,{recursive : true},(err) =>{
+        if(err) throw console.log("An error has occured pls try again");
+        console.log("assets / fonts folder has been successfully created 🥳");
+    }  )
+}
+
 
 /* creates files */
 let fileCreator = () =>{
-    fs.writeFileSync('index.html','', () => console.log("index.html has been created 🥳"));
+    fs.writeFileSync('index.html','',     () => console.log("index.html has been created 🥳"));
     fs.writeFileSync('assets/js/main.js','', () => console.log("main.js has been created 🥳"));
     fs.writeFileSync('assets/css/style.css','', () => console.log("style.css has been created 🥳"));
     fs.writeFileSync('assets/scss/style.scss','', () => console.log("style.scss has been created 🥳"));
@@ -40,4 +43,11 @@ let fileCreator = () =>{
     fs.writeFileSync('assets/scss/_variables.scss','', () => console.log("_variables.scss has been created 🥳"));
     fs.writeFileSync('assets/scss/_media-queries.scss','', () => console.log("_media-queries.scss has been created 🥳"));
 }
-fileCreator();
+
+for(let i = 0;i<=1;i++){
+    folderCreator();
+    fileCreator();
+    console.log(i + "test");
+}
+
+
